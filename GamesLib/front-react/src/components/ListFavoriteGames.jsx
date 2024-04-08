fe9dfe584cda4e0f1ea7e36bf0d7b3bfd8b3ee1d
Ariel André Aio <arielaio@hotmail.com>
2024-04-08 14:44:30 -0300

    Migrando todo o sistema

---
+++ GamesLib/front-react/src/components/ListFavoriteGames.jsx
@@ -0,0 +1,79 @@
+//Imports necessários
+import Slider from 'react-slick';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import 'slick-carousel/slick/slick.css'; 
+import 'slick-carousel/slick/slick-theme.css';
+
+import './Css/ListFavoriteGame.css';
+
+function ListFavoriteGames() {
+    const [favorites, setFavorites] = useState([]); // Define o estado para os jogos favoritos
+    const navigate = useNavigate(); // Permite a navegação programática entre rotas
+
+    useEffect(() => {
+        // Efeito que executa a busca dos jogos favoritos ao montar o componente
+        const fetchFavorites = async () => {
+            try {
+                const res = await fetch('http://127.0.0.1:8000/api/favorites'); // Faz uma requisição para obter os jogos favoritos
+                if (!res.ok) {
+                    throw new Error('Erro ao carregar jogos favoritos.'); // Lança um erro se a requisição falhar
+                }
+                const data = await res.json(); // Converte a resposta para JSON
+                setFavorites(data); // Define os jogos favoritos no estado
+            } catch (error) {
+                console.error('Erro:', error.message); // Exibe um erro no console em caso de falha
+            }
+        };
+
+        fetchFavorites(); // Chama a função para buscar os jogos favoritos
+    }, []);
+
+    const sliderSettings = {
+        // Configurações do Slider
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    };
+
+    // Função para navegar para os detalhes do jogo
+    const goToGameDetails = (game) => {
+        navigate(`/game/${game.slug}`, { state: { game } });
+    };
+
+    return (
+        <>
+            <h2 className="title-favorites">Jogos Favoritos:</h2> {/* Título da lista de jogos favoritos */}
+            <Slider {...sliderSettings}> {/* Componente Slider com as configurações definidas */}
+                {favorites.map((game) => (
+                    <div key={game.id} className="game-card" onClick={() => goToGameDetails(game)}> {/* Cartão de jogo individual */}
+                        <img src={game.background_image} alt={game.name} className="game-image" /> {/* Imagem do jogo */}
+                        <h3>{game.name}</h3> {/* Nome do jogo */}
+                    </div>
+                ))}
+            </Slider>
+        </>
+    );
+
+};
+
+export default ListFavoriteGames;
---
+++ GamesLib/front-react/src/components/ListGames.jsx
@@ -0,0 +1,101 @@
+//Imports necessários
+import React, { useState, useEffect } from 'react';
+import { FaStar } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
+import { BiSearchAlt2 } from 'react-icons/bi';
+
+import './Css/ListGames.css';
+
+function ListGames() {
+    const [games, setGames] = useState([]); // Define o estado games e a função setGames
+    const [isLoading, setIsLoading] = useState(false); // Define o estado isLoading e a função setIsLoading
+    const [searchTerm, setSearchTerm] = useState(''); // Define o estado searchTerm e a função setSearchTerm
+    const navigate = useNavigate(); // Define a função navigate com o hook useNavigate
+
+    useEffect(() => {
+        const fetchGames = async () => {
+            setIsLoading(true); // Define isLoading como true enquanto carrega os jogos
+            try {
+                const res = await fetch('http://127.0.0.1:8000/api'); // Faz uma requisição para obter os jogos
+                if (!res.ok) {
+                    throw new Error('Não foi possível carregar os jogos.'); // Lança um erro se não for possível carregar os jogos
+                }
+                const data = await res.json(); // Converte a resposta para JSON
+                setGames(data); // Define os jogos no estado
+            } catch (error) {
+                console.error('Erro:', error.message); // Exibe um erro no console em caso de falha
+            }
+            setIsLoading(false); // Define isLoading como false após carregar os jogos
+        };
+        fetchGames(); // Chama a função para carregar os jogos
+    }, []);
+
+    // Função para navegar para os detalhes do jogo
+    const goToGameDetails = (game) => {
+        navigate(`/game/${game.slug}`, { state: { game } });
+    };
+
+    // Função para lidar com a pesquisa de jogos
+    const handleSearch = async (event) => {
+        event.preventDefault(); // Previne o comportamento padrão do formulário
+        setIsLoading(true); // Define isLoading como true enquanto realiza a pesquisa
+        try {
+            const res = await fetch(`http://127.0.0.1:8000/api/search/${searchTerm}`); // Faz uma requisição para buscar os jogos com base no termo de pesquisa
+            if (!res.ok) {
+                throw new Error('Erro na busca dos jogos.'); // Lança um erro se não for possível realizar a busca
+            }
+            const data = await res.json(); // Converte a resposta para JSON
+            setGames(data); // Define os jogos encontrados no estado
+        } catch (error) {
+            console.error('Erro:', error.message); // Exibe um erro no console em caso de falha
+        }
+        setIsLoading(false); // Define isLoading como false após realizar a pesquisa
+    };
+
+    return (
+        <div className="container">
+
+            <h1 className="title">Buscar</h1>
+            <form onSubmit={handleSearch}>
+                <input
+                    type="text"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                    placeholder="Pesquisar jogos..."
+                />
+                <button type="submit">
+                    <BiSearchAlt2 />
+                </button>
+            </form>
+            {isLoading && (
+                <div className="loading-container">
+                    <div className="spinner"></div>
+                </div>
+            )}
+
+            <div className='games-container'>
+                {games.map((game, index) => (
+                    <div key={game.id} className="game-card" onClick={() => goToGameDetails(game)}>
+                        <h2 className='nameGame'>{game.name}</h2>
+                        {game.background_image && (
+                            <img src={game.background_image} alt="Imagem do jogo" />
+                        )}
+                        {game.rating !== undefined && (
+                            <p className='rate'>
+                                <FaStar className='star' /> {game.rating}
+                            </p>
+                        )}
+                        <div className="genre-card">
+                            {game.genres.map(genre => (
+                                <li className="genre">{genre}</li>
+                            ))}
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+
+};
+
+export default ListGames;
