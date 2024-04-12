c8335dbe0810230f3438e71d5b243fc76e277590
ArielAioES <ariel.aio@enterscience.com.br>
2024-04-12 13:21:40 -0300

    refatorando código de wishlist

--- GamesLib/back-laravel/app/Http/Controllers/GameController.php
+++ GamesLib/back-laravel/app/Http/Controllers/GameController.php
@@ -6,7 +6,6 @@ use Illuminate\Http\Request;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\RequestException;
 use Illuminate\Support\Facades\DB;
-use App\Models\Wishlist;
 
 // Controlador para lidar com as requisições relacionadas aos jogos
 class GameController extends Controller
@@ -221,58 +220,4 @@ class GameController extends Controller
         }
     }
     
-    public function addWishList(Request $request)
-    {
-        // Validação dos dados recebidos
-        $request->validate([
-            'name_game' => 'required|string|max:255',
-        ]);
-
-        // Recebendo o nome do jogo do corpo da requisição
-        $nameGame = $request->input('name_game');
-
-        // Verificando se o nome do jogo já existe na lista de desejos
-        $existingGame = DB::table('wishlist')->where('name_game', $nameGame)->first();
-
-        if ($existingGame) {
-            // Se o jogo já estiver na lista, retorne uma resposta de erro
-            return response()->json(['error' => 'O jogo já está na lista de desejos'], 400);
-        }
-
-        // Adicionando o nome do jogo à tabela 'wishlist' no banco de dados
-        DB::table('wishlist')->insert([
-            'name_game' => $nameGame,
-        ]);
-
-        // Retornando uma resposta de sucesso
-        return response()->json(['message' => 'Jogo adicionado à lista de desejos com sucesso']);
-    }
-
-    public function listGames()
-    {
-        // Recupere todos os dados da tabela wishlist
-        $wishlistItems = Wishlist::all();
-
-        // Verifique se existem itens na lista de desejos
-        if ($wishlistItems->isEmpty()) {
-            // Se não houver itens, retorne uma resposta com status 404 (Not Found)
-            return response()->json(['message' => 'Nenhum item encontrado na lista de desejos'], 404);
-        }
-
-        // Se houver itens na lista de desejos, retorne uma resposta com os itens
-        return response()->json($wishlistItems);
-    }
-
-    public function removeFromWishlist($id)
-    {
-        $game = Wishlist::find($id);
-
-        if (!$game) {
-            return response()->json(['message' => 'Jogo não encontrado na lista de desejos'], 404);
-        }
-
-        $game->delete();
-
-        return response()->json(['message' => 'Jogo removido da lista de desejos'], 200);
-    }
 }
--- GamesLib/back-laravel/routes/api.php
+++ GamesLib/back-laravel/routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 use App\Http\Controllers\GameController;
 use App\Http\Controllers\UserController;
-use App\Http\Controllers\LoginController;
+use App\Http\Controllers\WishlistController;
 
 // Rotas para lidar com requisições relacionadas aos jogos na API
 Route::get('/api', [GameController::class, 'home']); // Rota para a página inicial
@@ -13,9 +13,9 @@ Route::get('/api/favorites', [GameController::class, 'favorites']); // Rota para
 Route::get('/api/game/{query}', [GameController::class, 'select']); // Rota para selecionar um jogo
 
 // Rotas da lista de desejo
-Route::post('/api/wishlist', [GameController::class, 'addWishList']);
-Route::get('/api/wishlist', [GameController::class, 'listGames']);
-Route::delete('/api/wishlist/{id}', [GameController::class, 'removeFromWishlist']);
+Route::post('/api/wishlist', [WishlistController::class, 'addWishList']);
+Route::get('/api/wishlist', [WishlistController::class, 'listGames']);
+Route::delete('/api/wishlist/{id}', [WishlistController::class, 'removeFromWishlist']);
 
 // Rotas do CRUD do usuário
 Route::apiResource('/api/users', UserController::class);
